b5f530bce70c1bed8c48bab1074bdcdf815a771b
Yashraj Pravin Desale <119423317+BKYashraj@users.noreply.github.com>
2023-12-26 17:16:15 +0530

    follower change

--- ReactRouter/src/components/Youtube/youtube.jsx
+++ ReactRouter/src/components/Youtube/youtube.jsx
@@ -13,7 +13,10 @@ function youtube() {
   return (
     <h1 className=' text-center m-4 text-3xl p-6  bg-gray-700 text-white
     '>Github Followers : {data.followers} 
-    <img src={data.avatar_url} alt="" className=' p-4 'width={300}/>
+      <br />
+      <br />
+      Github Followings : {data.following} 
+    <img src={data.avatar_url} alt="" className=' p-4 'width={400}/>
     </h1>
   )
 }
